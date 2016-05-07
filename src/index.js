8d9bab6fe06c5a18d8d426cb00f888fd13ed7677
Ollipekka Jalonen <ollipekka.jalonen@gmail.com>
2016-05-07 21:27:35 +0300

    added timer example to test out boilerplate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
 
+const appState = new class AppState {
+  @observable timer = 0;
+
+  constructor() {
+    setInterval(() => {
+      appState.timer += 1;
+    }, 1000);
+  }
+
+  resetTimer() {
+    this.timer = 0;
+  }
+}();
+
+@observer
 class App extends Component {
+  static propTypes = {
+    appState: PropTypes.object,
+  }
+
+  onReset = () => {
+    this.props.appState.resetTimer();
+  }
+
   render() {
-    return <h1>Hello world!</h1>;
+    return (
+      <div>
+        <button onClick={this.onReset}>
+          Seconds passed: {this.props.appState.timer}
+        </button>
+        <DevTools />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App appState={appState} />, document.getElementById('app'));
