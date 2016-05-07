75dbc06c1c2803236625aee82b9aa1986479818f
Ollipekka Jalonen <ollipekka.jalonen@gmail.com>
2016-05-07 20:32:22 +0300

    Babel settings, eslint settings adjusted, webpack configurations, basic index.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,54 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
   app: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const common = {
   entry: {
     app: PATHS.app,
   },
 
   output: {
     path: PATHS.build,
-    filename: 'scripts.js',
+    filename: 'app.js',
+  },
+  resolve: {
+    extension: ['', '.js', '.jsx'],
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src'),
+    }],
   },
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT || 3000,
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
+
